37568315489ad1e30e03659557e5bfba9ebf1c98
KevinAp-5 <keven.clash3@gmail.com>
2020-09-22 00:08:05 -0300

    get_argv()

--- pomodoro.py
+++ pomodoro.py
@@ -4,5 +4,21 @@ from time import sleep, time
 from itertools import zip_longest
 
 def get_argv():
-    pass
+    conf = argv
+    conf.pop(0)  # Remove the python file name
+
+    if len(conf) == 1: conf.append(5)
+    if len(conf) == 0: return {'work-time':25.0, 'short-break':5.0}
+
+    conf = (float(x) for x in conf)  # Int the conf items(argv)
+    times = 'work-time', 'short-break', 'long-break'
+    config = dict(zip_longest(times, conf, fillvalue=0))
+
+    if 0 in config.values():
+        try:
+            config.pop(list(config.keys())[list(config.values()).index(0)])
+            # Remove the key if the value is 0
+        except Exception:
+            pass
+    return config
 
