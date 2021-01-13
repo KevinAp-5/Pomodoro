0194f9470cf77a7fda8b35c10fa1a7c2823e16ac
KevinAp-5 <keven.clash3@gmail.com>
2021-01-13 05:39:12 -0200

    setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from os import path
+readme_file = path.join(path.dirname(path.abspath(__file__)), 'README.md')
+
+try:
+    from m2r import parse_from_file
+    readme = parse_from_file(readme_file)
+except ImportError:
+    # m2r may not be installed in user environment
+    with open(readme_file) as f:
+        readme = f.read()
+
+setup(
+    name='Pomodoro',
+    version='1.2.0',
+    url='https://github.com/KevinAp-5/Pomodoro',
+    license='MIT License',
+    author='KevinAp-5',
+    author_email='keven.santos@protonmail.com',
+    keywords='Pomodoro pomodoro pomodoro_clock TomatoTimer python_3.6.x',
+    description='A simple way to use Pomodoro technique with python!',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=['Pomodoro'],
+    install_requires=['playsound', 'notify2'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+)
