d7c1265375be973cf73bed6a0e516ca3c602a7c2
KevinAp-5 <keven.clash3@gmail.com>
2023-06-16 00:51:12 -0300

    print_line()
    the separator: ----- is now replaced by a fund that will calculate the size of the terminal, and the print will follow 75% of the terminal size

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from pomodoro import times, config_extractor, show_config, run_configs
 from sysinfo import get_argv
+from os import get_terminal_size
 
 
 def main():
+    def print_line():
+        size = int(get_terminal_size()[0]*0.75)
+        print('-'*size)
+
     config = times(get_argv())
 
     long_config = config_extractor(config)
@@ -14,13 +19,13 @@ def main():
             show_config(config)
         run_configs(config)
         print(f'Counter: {i+1}')
-        print('-'*50)
+        print_line()
 
     show_config(long_config)
     run_configs(long_config)
 
     print('\nPomodoro is done!')
-    print('-' * 50)
+    print_line()
 
 
 if __name__ == '__main__':
