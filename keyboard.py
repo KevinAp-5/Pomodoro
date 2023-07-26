fcbe3df706d592a0cff6dffa30ef578b652340bf
KevinAp-5 <keven.clash3@gmail.com>
2023-07-25 23:07:02 -0300

    get_answer() method

--- keyboard.py
+++ keyboard.py
@@ -1,3 +1,15 @@
 class Get_input():
     def __init__(self):
         self.text = '\nDo you want to continue? [Y/n]\n>>> '
+
+    def get_answer(self):
+        while True:
+            try:
+                self.text = input(self.text).strip().lower()[0]
+            except KeyboardInterrupt:
+                exit()
+            except IndexError:
+                continue
+
+            if self.text:
+                return self.text
